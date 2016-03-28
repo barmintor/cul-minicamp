25368cbbced89ef81907e82ef5ac145d400fbbf3
Benjamin Armintor <armintor@gmail.com>
2016-03-28 07:37:54 -0400

    works - hydra:works model includes

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,5 @@
 class Work < ActiveFedora::Base
+  include Hydra::Works::WorkBehavior
   include Cul::Minicamp::LegacyProperties
   include Cul::Minicamp::CoreProperties
   contains "ocr", class_name: "ActiveFedora::File"
