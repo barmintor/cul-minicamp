4f4e7417525aa5ba01657e9515b757871477f0ed
Benjamin Armintor <armintor@gmail.com>
2016-03-30 06:57:59 -0400

    works - ocr is a GenericFile not a File

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,5 +2,5 @@ class Work < ActiveFedora::Base
   include Hydra::Works::WorkBehavior
   include Cul::Minicamp::LegacyProperties
   include Cul::Minicamp::CoreProperties
-  contains "ocr", class_name: "ActiveFedora::File"
+  contains "ocr", class_name: "GenericFile"
 end
